8851716110f9880e7b15ee9c9c32a927bf9b53a1
Rotich <elgeiy8@gmail.com>
2025-09-26 23:41:02 +0300

    launche

--- script.js
+++ script.js
@@ -93,3 +93,19 @@ if (quoteForm) {
 } else {
     console.error('Quote form not found');
 }
+
+// Hero slideshow
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000); // Change slide every 5 seconds
